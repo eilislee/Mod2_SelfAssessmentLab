980f7cdaf2dab67ed6c9d1a3f5f2132f272428d8
Eilis Lee <eilislee@live.com>
2019-02-23 11:14:01 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2SelfAssessmentLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int row = 0;
+
+            for (row = 0; row < 8; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    Console.WriteLine("X O X O X O X O");
+                }
+                else  // the remainder was not 0 so the value entere is an odd number.
+                {
+                    Console.WriteLine("O X O X O X O X");
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
